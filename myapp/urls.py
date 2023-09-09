7849746af2db9452b62f8485eb84e5851537383f
jeremiasolivera <jeremiasolivera166@gmail.com>
2023-09-09 00:31:07 -0300

    sitio restringido para desconocidos

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('registrar_paciente', views.extract_faces, name='extracted'),
     path('eliminar_paciente/<int:pk>', views.patients_delete, name='eliminar_paciente'),
     path('actualizar_paciente/<int:pk>', views.patients_update, name='actualizar_paciente'),
+    path('sitio_restringido/', views.restricted_site, name='sitio_restringido'),
     path('recognition/', views.recognition, name='recognition'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -5,6 +5,7 @@ import face_recognition
 from .models import Paciente
 from django.contrib.auth.decorators import login_required
 from .form import PacienteForm
+import time
 
 @login_required
 def extract_faces(request):
@@ -13,7 +14,7 @@ def extract_faces(request):
         patient_age = int(request.POST.get('patient_age'))
         patient_report = request.POST.get('patient_report')
         patient_name = request.POST.get('patient_name')  # Obtén el nombre del paciente del formulario
-        imagesPath = "C:/Users/alexi/OneDrive/Escritorio/Programming/django_recognition/core/faces"
+        imagesPath = "C:/Users/alexi/OneDrive/Escritorio/Programming/FaceRecognitionV2/faces"
 
         if not os.path.exists(imagesPath):
             os.makedirs(imagesPath)
@@ -29,6 +30,7 @@ def extract_faces(request):
         faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
         image = cv2.imread(image_path)
+        
         faces = faceClassif.detectMultiScale(image, 1.1, 5)
         for (x, y, w, h) in faces:
             face = image[y:y + h, x:x + w]
@@ -45,7 +47,7 @@ def extract_faces(request):
 
 @login_required
 def recognition(request):
-        imageFacesPath = "C:/Users/alexi/OneDrive/Escritorio/Programming/django_recognition/core/faces"
+        imageFacesPath = "C:/Users/alexi/OneDrive/Escritorio/Programming/FaceRecognitionV2/faces"
 
         facesEncodings = []
         facesNames = []
@@ -141,3 +143,139 @@ def patients_update(request, pk):
         form = PacienteForm(instance=paciente)
 
     return render(request, 'pages/actualizar_paciente.html', {'form': form})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##########################################
+#         Codigo funcionando
+##########################################
+
+@login_required
+def restricted_site(request):
+    if request.method == 'POST':
+
+        time_desc_advertencia = int(request.POST.get('tiempo_desconocido'))
+        print(time_desc_advertencia)
+        # Ruta donde se encuentran las imágenes de referencia de rostros conocidos
+        imageFacesPath = "C:/Users/alexi/OneDrive/Escritorio/Programming/FaceRecognitionV2/faces"
+
+        # Listas para almacenar las codificaciones de los rostros conocidos y sus nombres
+        facesEncodings = []
+        facesNames = []
+
+        # Cargar las codificaciones de los rostros conocidos
+        for file_name in os.listdir(imageFacesPath):
+            image = cv2.imread(os.path.join(imageFacesPath, file_name))
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            
+            # Obtener la codificación del rostro
+            f_coding = face_recognition.face_encodings(image, known_face_locations=[(0, 150, 150, 0)])[0]
+            
+            facesEncodings.append(f_coding)
+            facesNames.append(file_name.split(".")[0])
+
+        # Inicializar la cámara para la detección en tiempo real
+        cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+        # Detector facial
+        faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+        # Inicializar una bandera para indicar si se detectó un rostro desconocido
+        desconocido_detectado = False
+        conocido_detectado = False
+
+        start_time = None
+
+        while True:
+            # Capturar un fotograma de la cámara
+            ret, frame = cap.read()
+
+            if not ret:
+                break
+
+            # Voltear el fotograma horizontalmente
+            frame = cv2.flip(frame, 1)
+            orig = frame.copy()
+
+            # Detectar rostros en el fotograma
+            faces = faceClassif.detectMultiScale(frame, 1.1, 5)
+
+            for (x, y, w, h) in faces:
+                face = orig[y:y + h, x:x + w]
+                face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+                
+                # Obtener la codificación del rostro detectado
+                actual_face_encoding = face_recognition.face_encodings(face, known_face_locations=[(0, w, h, 0)])[0]
+                
+                # Comparar la codificación del rostro detectado con las codificaciones de los rostros conocidos
+                result = face_recognition.compare_faces(facesEncodings, actual_face_encoding)
+
+                if True in result:
+                    # Si se encuentra un rostro conocido, obtener su nombre
+                    index = result.index(True)
+                    name = facesNames[index]
+                    color = (125, 220, 0)
+                    # Reiniciar el contador de tiempo si se detecta un rostro conocido
+                    start_time = None
+                else:
+                    # Si el rostro es desconocido, mostrar "Desconocido" y cambiar el color
+                    name = "Desconocido"
+                    color = (50, 50, 255)
+                    # Iniciar el contador de tiempo si aún no se ha iniciado
+                    if start_time is None:
+                        start_time = time.time()
+                    elif time.time() - start_time >= time_desc_advertencia and desconocido_detectado:
+                        # Si se ha superado el tiempo límite de 10 segundos después de detectar un rostro desconocido, salir del bucle
+                        desconocido_detectado = True
+                        break
+                    elif time.time() - start_time >= time_desc_advertencia and desconocido_detectado and conocido_detectado:
+                        # Si se ha superado el tiempo límite de 10 segundos después de detectar un rostro desconocido, salir del bucle
+                        desconocido_detectado = True
+                        break
+                    elif time.time() - start_time < time_desc_advertencia and not desconocido_detectado:
+                        desconocido_detectado = True
+
+                cv2.rectangle(frame, (x, y + h), (x + w, y + h + 30), color, -1)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+                cv2.putText(frame, name, (x, y + h + 25), 2, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # Mostrar el fotograma con los resultados
+            cv2.imshow("Frame", frame)
+
+            # Comprobar si se ha superado el tiempo límite de time_desc_advertencia segundos
+            if start_time is not None and time.time() - start_time >= time_desc_advertencia:
+                break
+
+            # Esperar la tecla ESC para salir
+            k = cv2.waitKey(1)
+            if k == 27:
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+        # Devolver los resultados a la plantilla
+        context = {
+            'result': result,
+            'desconocido_detectado': desconocido_detectado,  # Agregar los resultados del reconocimiento
+        }
+
+        # Renderizar la plantilla con los resultados
+        return render(request, 'pages/sitio_restringido.html', context)
+    else:
+        return render(request, 'pages/sitio_restringido.html')
